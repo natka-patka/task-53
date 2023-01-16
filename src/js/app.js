3a041e1b130071c3214e4a0eb1c407fadbde55e2
natkapatka <pencheva_nat@yahoo.com>
2023-01-16 22:47:12 +0200

    rw
    rew

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,7 @@
 import "../scss/app.scss";
-
+function myFunction() {
+  document.getElementsByClassName(".product").setAttribute("data-price"); 
+}
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
