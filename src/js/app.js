967d90491719da18d2941fd97d0186ee2dd6580a
natkapatka <pencheva_nat@yahoo.com>
2023-01-16 23:23:21 +0200

    rwe
    rwe

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 // import "../scss/app.scss";
 function myFunction() {
   var element = document.getElementsByClassName(".product");
-  element.classList.add("data-price");
+  element.attribute.add("data-price");
 }
 // window.addEventListener("DOMContentLoaded", () => {
 //   // This block will be executed once the page is loaded and ready
