9c76c4926368377cdfc4093562ccce362b0d9cec
natkapatka <pencheva_nat@yahoo.com>
2023-01-16 23:00:38 +0200

    fsd
    dfs

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,8 @@ import "../scss/app.scss";
 function myFunction() {
   document.getElementsByClassName(".product").setAttribute("data-price"); 
 }
-window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+// window.addEventListener("DOMContentLoaded", () => {
+//   // This block will be executed once the page is loaded and ready
 
 
-});
+// });
